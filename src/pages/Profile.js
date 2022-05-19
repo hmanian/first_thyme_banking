b6b67f1930b1501fa9c977d924adf202caf0290e
hmanian <hmanian@purdue.edu>
2022-05-19 09:34:47 -0700

    first push

---
+++ src/pages/Profile.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Profile.css";
+
+const Profile = () => {
+    return (
+        <div className="Profile">
+            <h2>profile</h2>
+        </div>
+    );
+}
+
+export default Profile;
---
+++ src/pages/Walc.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Walc.css";
+
+const Walc = () => {
+    return (
+        <div className="Walc">
+            <h2>walc</h2>
+        </div>
+    );
+}
+
+export default Walc;
