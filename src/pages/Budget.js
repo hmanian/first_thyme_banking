4e02a3cda3476ef7b874416d4c63468c91a242ec
Ramya Chitturi <ramya.chitturi@berkeley.edu>
2022-05-19 12:52:46 -0700

    initial profile done

--- src/pages/Budget.js
+++ src/pages/Budget.js
@@ -4,9 +4,6 @@ const Budget = () => {
     return (
         <div className="Budget">
             <h2>Parrish Library</h2>
-            <h3 id="parrishAvailability">Availability</h3>
-            <h3 id="parrishFood">Food Options Nearby</h3>
-            <h3 id="parrishVolume">Volume Surrounding the Room</h3>
         </div>
     );
 }
