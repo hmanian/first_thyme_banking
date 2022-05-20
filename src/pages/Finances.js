b4b4a10a1095867eeaab101c14ec0779e918b06d
Juan Pablo U <jurista@andrew.cmu.edu>
2022-05-19 22:03:14 -0500

    remove finance filters and update profile input

--- src/pages/Finances.js
+++ src/pages/Finances.js
@@ -7,33 +7,7 @@ const Finances = () => {
     return (
         <div className="Finances">
                 <h2>Financial Information</h2>
-
-                <div className="filter">
-                        <div className="filterOption">
-                                <label>Category</label>
-                                <select id="filterCat" onChange="checkCat()">
-                                        <option value="" disabled>Select</option>
-                                        <option value="Banking">Banking</option>
-                                        <option value="Budgeting">Budgeting</option>
-                                        <option value="Credit">Credit</option>
-                                        <option value="Investing">Investing</option>
-                                        <option value="Other">Other</option>
-                                </select>
-                        </div>
-
-                        <div className="filterOption">
-                                <label>Level</label>
-                                <select id="filterLevel" onChange="checkLevel">
-                                        <option value="" disabled>Select</option>
-                                        <option value="Beginner">Beginner</option>
-                                        <option value="Intermediate">Intermediate</option>
-                                        <option value="Expert">Expert</option>
-                                </select>
-                        </div>
-                        
-                </div>
-
-                <div className='articles'>
+                <div className='articles'> 
                         {data.map((article) => (
                         <Article title = {article.title} link = {article.link} genre = {article.genre} level = {article.level}  publisher = {article.publisher}/>
                         ))}
